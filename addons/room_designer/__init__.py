37922069bf6ca64fd83700099246e664176cf122
Andyrexic <dev.andrewpeel@gmail.com>
2017-07-26 19:29:05 -0700

    Replacing Blenders Default Interface

--- addons/room_designer/__init__.py
+++ addons/room_designer/__init__.py
@@ -31,43 +31,8 @@ bl_info = {
 import bpy
 from . import room_designer
 
-def clear_blender_panels():
-    if hasattr(bpy.types, 'VIEW3D_PT_grease_pencil'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_grease_pencil)
-    if hasattr(bpy.types, 'VIEW3D_PT_grease_pencil_palettecolor'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_grease_pencil_palettecolor)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_properties'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_properties)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_cursor'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_cursor)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_name'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_name)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_display'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_display)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_stereo'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_stereo)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_shading'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_shading)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_motion_tracking'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_motion_tracking)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_meshdisplay'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_meshdisplay)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_meshstatvis'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_meshstatvis)
-    if hasattr(bpy.types, 'VIEW3D_PT_view3d_curvedisplay'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_view3d_curvedisplay)
-    if hasattr(bpy.types, 'VIEW3D_PT_background_image'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_background_image)
-    if hasattr(bpy.types, 'VIEW3D_PT_transform_orientations'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_transform_orientations)
-    if hasattr(bpy.types, 'VIEW3D_PT_etch_a_ton'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_etch_a_ton)
-    if hasattr(bpy.types, 'VIEW3D_PT_context_properties'):
-        bpy.utils.unregister_class(bpy.types.VIEW3D_PT_context_properties)
-
 def register():
-    clear_blender_panels()
-    room_designer.register()
+    pass
 
 def unregister():
     pass
